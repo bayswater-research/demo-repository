2efa837a36d3cea0ababe2ae341c16200ad5f9a3
theFAY <thefay@theFAYs-MacBook-Air.fritz.box>
2026-06-27 18:44:56 +0200

    change vite config

--- bayswater-github-showcase/vite.config.ts
+++ bayswater-github-showcase/vite.config.ts
@@ -5,6 +5,10 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: './',
+    build: {
+      outDir: '../',
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
